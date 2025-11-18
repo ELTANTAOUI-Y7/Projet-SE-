5fd0d3b14928a81bf69930aa037838bd38953e0a
Amine Benali <Amine.BENALI@um6p.ma>
2025-11-18 16:05:52 +0100

    Added tests

---
+++ src/test/java/com/mycompany/phone_shoop/Dao/CategoryDaoTest.java
@@ -0,0 +1,71 @@
+package com.mycompany.phone_shoop.Dao;
+
+import com.mycompany.phone_shoop.entities.Category;
+import java.util.Arrays;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class CategoryDaoTest {
+
+    private SessionFactory factory;
+    private Session session;
+    private Transaction transaction;
+    private CategoryDao categoryDao;
+
+    @Before
+    public void setUp() {
+        factory = mock(SessionFactory.class);
+        session = mock(Session.class);
+        transaction = mock(Transaction.class);
+        when(factory.openSession()).thenReturn(session);
+        when(session.beginTransaction()).thenReturn(transaction);
+        categoryDao = new CategoryDao(factory);
+    }
+
+    @Test
+    public void saveCategoryReturnsGeneratedId() {
+        Category category = new Category();
+        when(session.save(category)).thenReturn(15);
+
+        int id = categoryDao.saveCategory(category);
+
+        assertEquals(15, id);
+        verify(transaction).commit();
+        verify(session).close();
+    }
+
+    @Test
+    public void getCategoriesReturnsAllCategories() {
+        Query query = mock(Query.class);
+        List<Category> categories = Arrays.asList(new Category(), new Category());
+        when(session.createQuery("from Category")).thenReturn(query);
+        when(query.list()).thenReturn(categories);
+
+        List<Category> result = categoryDao.getCategories();
+
+        assertSame(categories, result);
+        verify(session).close();
+    }
+
+    @Test
+    public void getCategoryByIdReturnsMatchingCategory() {
+        Category category = new Category();
+        when(session.get(Category.class, 4)).thenReturn(category);
+
+        Category result = categoryDao.getCategoryById(4);
+
+        assertSame(category, result);
+        verify(session).close();
+    }
+}
+
---
+++ src/test/java/com/mycompany/phone_shoop/Dao/ProductDaoTest.java
@@ -0,0 +1,76 @@
+package com.mycompany.phone_shoop.Dao;
+
+import com.mycompany.phone_shoop.entities.Product;
+import java.util.Arrays;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ProductDaoTest {
+
+    private SessionFactory factory;
+    private Session session;
+    private Transaction transaction;
+    private ProductDao productDao;
+
+    @Before
+    public void setUp() {
+        factory = mock(SessionFactory.class);
+        session = mock(Session.class);
+        transaction = mock(Transaction.class);
+        when(factory.openSession()).thenReturn(session);
+        when(session.beginTransaction()).thenReturn(transaction);
+        productDao = new ProductDao(factory);
+    }
+
+    @Test
+    public void saveProductPersistsEntity() {
+        Product product = new Product();
+
+        boolean result = productDao.saveProduct(product);
+
+        verify(session).save(product);
+        verify(transaction).commit();
+        verify(session).close();
+        // method should indicate persistence succeeded
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void getAllProductsReturnsListFromQuery() {
+        Query query = mock(Query.class);
+        List<Product> products = Arrays.asList(new Product(), new Product(), new Product());
+        when(session.createQuery("from Product")).thenReturn(query);
+        when(query.list()).thenReturn(products);
+
+        List<Product> result = productDao.getAllProducts();
+
+        assertSame(products, result);
+        verify(session).close();
+    }
+
+    @Test
+    public void getAllProductsByCatIdFiltersByCategoryId() {
+        Query query = mock(Query.class);
+        List<Product> products = Arrays.asList(new Product());
+        when(session.createQuery("from Product as p where p.category.categoryId =: id")).thenReturn(query);
+        when(query.setParameter("id", 7)).thenReturn(query);
+        when(query.list()).thenReturn(products);
+
+        List<Product> result = productDao.getAllProductsByCatId(7);
+
+        assertSame(products, result);
+        verify(query).setParameter("id", 7);
+        verify(session).close();
+    }
+}
+
