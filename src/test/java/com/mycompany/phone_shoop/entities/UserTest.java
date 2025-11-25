496540c5889145ceb81eb744aad222b101bda749
Mohamed.AKSIKAS@um6p.ma <Mohamed.AKSIKAS@um6p.ma>
2025-11-25 17:37:20 +0100

    more tests

--- src/test/java/com/mycompany/phone_shoop/entities/UserTest.java
+++ src/test/java/com/mycompany/phone_shoop/entities/UserTest.java
@@ -40,5 +40,27 @@ public class UserTest {
         // userId stays default
         assertEquals(0, user.getUserId());
     }
+
+    @Test
+    public void gettersReturnPreviouslySetValues() {
+        User user = new User();
+        user.setUserId(12);
+        user.setUserName("Alex");
+        user.setUserEmail("alex@mail.com");
+        user.setUserPassword("pwd");
+        user.setUserPhone("000");
+        user.setUserPic("pic.png");
+        user.setUserAddress("planet");
+        user.setUserType("admin");
+
+        assertEquals(12, user.getUserId());
+        assertEquals("Alex", user.getUserName());
+        assertEquals("alex@mail.com", user.getUserEmail());
+        assertEquals("pwd", user.getUserPassword());
+        assertEquals("000", user.getUserPhone());
+        assertEquals("pic.png", user.getUserPic());
+        assertEquals("planet", user.getUserAddress());
+        assertEquals("admin", user.getUserType());
+    }
 }
 
